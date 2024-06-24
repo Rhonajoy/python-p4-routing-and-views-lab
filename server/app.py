55cb89aaef6526b00b35a8264068482e86127adf
RhonaJoy <rhonajoy8@gmail.com>
2024-06-24 12:49:49 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -3,6 +3,12 @@
 from flask import Flask
 
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
 
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>print {username}</h1>'
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
